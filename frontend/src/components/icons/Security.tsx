42f9a1b95d031d37ee4aa6c3ef7b5b37fec63460
Akifcan <akfkara97@gmail.com>
2025-09-27 20:22:49 +0300

    syntax fixes

--- frontend/src/components/icons/Security.tsx
+++ frontend/src/components/icons/Security.tsx
@@ -1,9 +1,9 @@
 export default function Security(){
     return <svg width="60" height="60" viewBox="0 0 104 104" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect width="104" height="104" rx="15" fill="#2D2DF1"/>
-<path d="M80 44H24V84H80V44Z" stroke="white" stroke-width="8" stroke-linejoin="round"/>
-<path d="M36 48C36 36 35.9995 20 51.9999 20C68.0002 20 68.0001 36 68.0001 48" stroke="white" stroke-width="8" stroke-linejoin="round"/>
-<path d="M52 60V68" stroke="white" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M80 44H24V84H80V44Z" stroke="white" strokeWidth="8" strokeLinejoin="round"/>
+<path d="M36 48C36 36 35.9995 20 51.9999 20C68.0002 20 68.0001 36 68.0001 48" stroke="white" strokeWidth="8" strokeLinejoin="round"/>
+<path d="M52 60V68" stroke="white" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 
 }
